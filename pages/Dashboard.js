bb3a3c4da753cb27549e22c0347c272a25de23d3
seedm24 <despotic62@gmail.com>
2025-01-22 14:49:36 +0300

    Update Dashboard.js

--- pages/Dashboard.js
+++ pages/Dashboard.js
@@ -1,1 +1,13 @@
+import React from "react";
+
+function Dashboard() {
+  return (
+    <div className="dashboard">
+      <h2>Dashboard</h2>
+      <p>Welcome to the VirtuLab Operations Dashboard!</p>
+    </div>
+  );
+}
+
+export default Dashboard;
 
