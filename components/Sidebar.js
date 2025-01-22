92d5190321c7b7af14ea9065263f17f66fe31038
seedm24 <despotic62@gmail.com>
2025-01-22 14:48:25 +0300

    Update Sidebar.js

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,1 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./../styles/Sidebar.css";
+
+function Sidebar() {
+  return (
+    <aside className="sidebar">
+      <ul>
+        <li><Link to="/dashboard">Dashboard</Link></li>
+        <li><Link to="/agents">Agents</Link></li>
+        <li><Link to="/">Logout</Link></li>
+      </ul>
+    </aside>
+  );
+}
+
+export default Sidebar;
 
