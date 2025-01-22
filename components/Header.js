e734098b475482d996ba6b40d0c3719799d37f96
seedm24 <despotic62@gmail.com>
2025-01-22 14:46:41 +0300

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,1 +1,13 @@
+import React from "react";
+import "./../styles/App.css";
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>VirtuLab Dashboard</h1>
+    </header>
+  );
+}
+
+export default Header;
 
