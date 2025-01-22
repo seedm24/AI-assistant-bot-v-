8fa1c1e03c5d6ee23ddeaf03c6ec3e234a7fe900
seedm24 <despotic62@gmail.com>
2025-01-22 17:52:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://ai-assistant-bot-vm.vercel.app"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
